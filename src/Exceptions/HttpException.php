b1ff5c2d5343c256f4a82b5154d75d928db5ffe3
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2016-11-18 23:59:55 +0100

    bug fix Http::fetchUserInfo

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -7,20 +7,19 @@
  * @author     Ignace Nyamagana Butera <nyamsprod@gmail.com>
  * @copyright  2016 Ignace Nyamagana Butera
  * @license    https://github.com/thephpleague/uri-parser/blob/master/LICENSE (MIT License)
- * @version    0.2.0
+ * @version    0.3.0
  * @link       https://github.com/thephpleague/uri-parser/
  */
 namespace League\Uri\Schemes\Exceptions;
 
 /**
- * a Trait to validate a Hostname
+ * Exception class for Http(s) Uri
  *
- * @see     https://tools.ietf.org/html/rfc3986
  * @package League\Uri
  * @author  Ignace Nyamagana Butera <nyamsprod@gmail.com>
- * @since   0.2.0
+ * @since   0.3.0
  */
-class HttpUriException extends UriException
+class HttpException extends UriException
 {
     /**
      * Returns a new Instance from an error in server configuration
