679ab4b77c4ec4ce220c6aaec8c15619bec36962
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2017-03-06 09:51:22 +0100

    prepare 1.0.3 release

--- src/AbstractUri.php
+++ src/AbstractUri.php
@@ -7,7 +7,7 @@
  * @author     Ignace Nyamagana Butera <nyamsprod@gmail.com>
  * @copyright  2016 Ignace Nyamagana Butera
  * @license    https://github.com/thephpleague/uri-components/blob/master/LICENSE (MIT License)
- * @version    1.0.0
+ * @version    1.0.3
  * @link       https://github.com/thephpleague/uri-components
  */
 declare(strict_types=1);
